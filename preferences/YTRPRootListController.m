5342825b2a56a730c0ab2d44145e273515f9c3de
Preloading <me@preloading.dev>
2026-02-14 17:14:51 -0700

    feat; the preferences menu

--- preferences/YTRPRootListController.m
+++ preferences/YTRPRootListController.m
@@ -2,6 +2,9 @@
 #import <Preferences/PSSpecifier.h>
 #import "YTRPRootListController.h"
 
+#define PREFS_PATH @"/var/mobile/Library/Preferences/dev.preloading.tubereplacer.preferences.plist"
+#define NOTIFY_NAME CFSTR("dev.preloading.tubereplacer.preferences/settingschanged")
+
 @implementation YTRPRootListController
 
 - (NSArray *)specifiers {
@@ -10,7 +13,7 @@
         
         // Check current preference value
         NSDictionary *prefs = [NSDictionary dictionaryWithContentsOfFile:@"/var/mobile/Library/Preferences/dev.preloading.tubereplacer.preferences.plist"];
-        NSString *selectedValue = prefs[@"StreamType"] ?: @"option1";
+        NSString *selectedValue = prefs[@"StreamType"] ?: @"adaptive";
         
         // Only show text field if "custom" is selected
         if (![selectedValue isEqualToString:@"custom"]) {
@@ -41,6 +44,80 @@
         _specifiers = nil;
         [self reloadSpecifiers];
     }
+}
+
+-(void)resetCategorySettings{
+    NSLog(@"Resetting category settings");
+
+    // Define all category keys and their default values (from Root.plist)
+    NSDictionary *defaultValues = @{
+        // Trending
+        @"TrendingBrowseId": @"VLPL-p0-Yh03xpi2AsCiyuafMeQrMF6czMoL",
+        // Film
+        @"FilmName": @"Film & Animation",
+        @"FilmBrowseId": @"VLPL-p0-Yh03xpgQgBqDn3T4EZbxoaYXkQjY",
+        // Auto
+        @"AutoName": @"Autos & Vehicles",
+        @"AutoBrowseId": @"VLPL-p0-Yh03xphS0WmPB1u5mQbRJjPRn63U",
+        // Music
+        @"MusicName": @"Music",
+        @"MusicBrowseId": @"VLPL-p0-Yh03xpgeN91B_sPpv4lJY-UfThEi",
+        // Animals
+        @"AnimalsName": @"Pets & Animals",
+        @"AnimalsBrowseId": @"VLPL-p0-Yh03xpgqRqXBDc9DbcCysUjd_CSB",
+        // Sports
+        @"SportsName": @"Sports",
+        @"SportsBrowseId": @"VLPL-p0-Yh03xpg6CLD7MDqzsAiB9aFjssWb",
+        // Travel
+        @"TravelName": @"Travel",
+        @"TravelBrowseId": @"",
+        // Games
+        @"GamesName": @"Gaming",
+        @"GamesBrowseId": @"VLPL-p0-Yh03xpi_x9L-Lqop_Kj6MTY38jqv",
+        // Comedy
+        @"ComedyName": @"Comedy",
+        @"ComedyBrowseId": @"VLPL-p0-Yh03xpj0Js3pnGO20BWWiHVn1oHz",
+        // People
+        @"PeopleName": @"People & Blogs",
+        @"PeopleBrowseId": @"VLPL-p0-Yh03xphi7-iBuIshu7olymbv7lY-",
+        // News
+        @"NewsName": @"News & Politics",
+        @"NewsBrowseId": @"VLPL-p0-Yh03xpgeG3YUmWESSrg84W8ELEUO",
+        // Entertainment
+        @"EntertainmentName": @"Entertainment",
+        @"EntertainmentBrowseId": @"VLPL-p0-Yh03xpjoqDAI46lgo8-TLDnE7mHF",
+        // Education
+        @"EducationName": @"Education",
+        @"EducationBrowseId": @"",
+        // Howto
+        @"HowtoName": @"Howto & Style",
+        @"HowtoBrowseId": @"VLPL-p0-Yh03xphCxNSaXOW09V3pKgRQCFvn",
+        // Nonprofit
+        @"NonprofitName": @"Nonprofits & Activism",
+        @"NonprofitBrowseId": @"",
+        // Tech
+        @"TechName": @"Science & Technology",
+        @"TechBrowseId": @"VLPL-p0-Yh03xpgQgBqDn3T4EZbxoaYXkQjY"
+    };
+
+    // Load current prefs
+    NSMutableDictionary *prefs = [NSMutableDictionary dictionaryWithContentsOfFile:PREFS_PATH];
+    if (!prefs) prefs = [NSMutableDictionary dictionary];
+
+    // Set defaults for all category keys
+    [defaultValues enumerateKeysAndObjectsUsingBlock:^(NSString *key, id value, BOOL *stop) {
+        prefs[key] = value;
+    }];
+
+    // Save back to disk
+    [prefs writeToFile:PREFS_PATH atomically:YES];
+
+    // Notify system of changes
+    CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), NOTIFY_NAME, NULL, NULL, YES);
+
+    // Reload UI
+    _specifiers = nil;
+    [self reloadSpecifiers];
 }
 
 @end
